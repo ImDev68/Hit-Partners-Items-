2362c36f2fa3c54c80560f9fa7b4d9584a89b058
imDev68 <84857287+ImDev68@users.noreply.github.com>
2022-05-05 11:01:09 -0500

    add ítems cooldown

--- src/partneritems/items/SpecialItems.php
+++ src/partneritems/items/SpecialItems.php
@@ -16,6 +16,7 @@ use pocketmine\item\ItemIds;
 use pocketmine\item\Item;
 
 use pocketmine\world\sound\AnvilFallSound;
+use pocketmine\world\sound\XpCollectSound;
 
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\event\Listener;
@@ -41,30 +42,40 @@ class SpecialItems implements Listener {
         $damager = $event->getDamager();
         $entity = $event->getEntity();
         if($entity instanceof Player && $damager instanceof Player && $damager->getInventory()->getItemInHand()->getCustomName() === "§r§b§lInventory Clogger"){
-         
+         if (!isset($this->globalCooldown[$damager->getName()])) {
+
+            $this->globalCooldown[$damager->getName()] = time() + 180;
+                
             $entity->getInventory()->addItem(ItemFactory::getInstance()->get(270, 0, 36));
           //MESAGGES 
           
             $entity->sendMessage("§l§7[§6!§7] - §r§7Your inventory was clogged since someone used the 'Inventory Clogger' item against you.");
             $damager->sendMessage("§l§7[§6!§7] - §r§7You have clogged the inventory of §f$entity.");
+            $damager->getWorld()->addSound($damager->getPosition(), new XpCollectSound());
+            
+          } else if (time() <= $this->globalCooldown[$damager->getName()]) {
+      
+                $reaming = $this->globalCooldown[$damager->getName()] - time();
+                $damager->sendMessage("§l§7[§6!§7] - §r§7You hare in §dglobal cooldown §r§7wait §f" . $reaming."§7s ");
+         } else {
+             unset($this->globalCooldown[$damager->getName()]);
+            
             $e = $damager->getInventory()->getItemInHand();
             $damager->getInventory()->removeItem($e);
         }
         if($entity instanceof Player && $damager instanceof Player && $damager->getInventory()->getItemInHand()->getCustomName() === "§r§a§lMedusa Ability"){
           
                      $entity->setImmobile(true);
-                     
                      $entity->sendMessage("\n§l§4❤ §r§c Oh no! You have been petrified by $damager wait 5 seconds to get back to normal \n");
-				     
-				               $this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($entity): void {
-					           if ($entity->isOnline()) {
-						            $entity->setImmobile(false);
-						            $entity->sendMessage("\n§l§4❤️ §r§6You've returned to normal thanks to Apollo's spell\n");
+				             $this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($entity): void {
+					       if ($entity->isOnline()) {
+						         $entity->setImmobile(false);
+						         $entity->sendMessage("\n§l§4❤️ §r§6You've returned to normal thanks to Apollo's spell\n");
 					           }
 					           }), 5 * 20);
-					           
-					       $e = $damager->getInventory()->getItemInHand();
-                 $damager->getInventory()->removeItem($e);
-        }
+					           $e = $damager->getInventory()->getItemInHand();
+                     $damager->getInventory()->removeItem($e);
+         } 
       }
-    }
+   }
+}
